dc2d4922b420fd6208831a0cb7a34162339fd34e
Raymond <raymond@philippinedev.com>
2022-01-31 01:24:42 +0800

    Initialize project using create-react-app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDTk8YMbY_i_reL0zG_fGp5un0nbM-pfmA",
+  authDomain: "build-netflix-5a4e7.firebaseapp.com",
+  projectId: "build-netflix-5a4e7",
+  storageBucket: "build-netflix-5a4e7.appspot.com",
+  messagingSenderId: "536016123709",
+  appId: "1:536016123709:web:8e953ea7ee3894aa363754",
+};
