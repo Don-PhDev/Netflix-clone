d24b5f592e38e41640a5df531d7c8d19ce75b3b6
Raymond <raymond@philippinedev.com>
2022-01-31 22:25:07 +0800

    Change genres

--- src/Requests.js
+++ src/Requests.js
@@ -1,16 +1,15 @@
 const API_KEY = "589a0bba7dc58714016e2ba181e8e1f5";
-const discoverTv = "/discover/tv?api_key=";
-const usLanguage = "&language=en-US";
 
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}${usLanguage}`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}${usLanguage}`,
-  fetchActionMovies: `${discoverTv}${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `${discoverTv}${API_KEY}&with_genres=35`,
-  fetchHorroMovies: `${discoverTv}${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `${discoverTv}${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `${discoverTv}${API_KEY}&with_genres=99`,
+  fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
+  fetchAnimatedMovies: `/discover/tv?api_key=${API_KEY}&with_genres=16`,
+  fetchSciFiFantasyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10765`,
+  fetchDramaMovies: `/discover/tv?api_key=${API_KEY}&with_genres=18`,
+  fetchWesternMovies: `/discover/tv?api_key=${API_KEY}&with_genres=37`,
+  fetchRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
 };
 
 export default requests;
