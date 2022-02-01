cfec632bd6ce28cecdb0ac77dd1fae50fce8cc25
Raymond <raymond@philippinedev.com>
2022-02-02 00:18:16 +0800

    Update imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDTk8YMbY_i_reL0zG_fGp5un0nbM-pfmA",
   authDomain: "build-netflix-5a4e7.firebaseapp.com",
@@ -6,3 +10,29 @@ const firebaseConfig = {
   messagingSenderId: "536016123709",
   appId: "1:536016123709:web:8e953ea7ee3894aa363754",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
+
+// import firebase from "./firebase";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDTk8YMbY_i_reL0zG_fGp5un0nbM-pfmA",
+//   authDomain: "build-netflix-5a4e7.firebaseapp.com",
+//   projectId: "build-netflix-5a4e7",
+//   storageBucket: "build-netflix-5a4e7.appspot.com",
+//   messagingSenderId: "536016123709",
+//   appId: "1:536016123709:web:8e953ea7ee3894aa363754",
+// };
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+
+// export { auth };
+// export default db;
