c235c22938b2518bf439cbbd2ddedc487d1734a1
Raymond <raymond@philippinedev.com>
2022-02-02 00:18:43 +0800

    Remember signed in user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./pages/HomeScreen";
 import LoginScreen from "./pages/LoginScreen";
 import "./App.css";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        console.log(userAuth);
+      } else {
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
