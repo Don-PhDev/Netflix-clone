f645e75b90c3a91fcd9266eecaf1901c76853c14
Raymond <raymond@philippinedev.com>
2022-02-02 00:19:50 +0800

    Set up Authentication

--- src/pages/SignUpScreen.js
+++ src/pages/SignUpScreen.js
@@ -1,17 +1,62 @@
+import { useRef } from "react";
+import { auth } from "../firebase";
 import "../styles/SignUpScreen.css";
 
 function SignUpScreen() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
   return (
     <div className="sign-up-screen">
       <form>
         <h1 className="sign-up-title">Sign In</h1>
-        <input type="email" placeholder="Email" className="sign-up-email" />
         <input
+          ref={emailRef}
+          type="email"
+          placeholder="Email"
+          className="sign-up-email"
+        />
+        <input
+          ref={passwordRef}
           type="password"
           placeholder="Password"
           className="sign-up-password"
         />
-        <button type="submit">Sign In</button>
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
       </form>
 
       <div className="remember-me">
@@ -21,7 +66,10 @@ function SignUpScreen() {
 
       <h4>
         <span>New to Netflix?</span>
-        <span className="sign-up-screen-white sign-up-link">Sign up now</span>.
+        <span className="sign-up-screen-white sign-up-link" onClick={register}>
+          Sign up now
+        </span>
+        .
       </h4>
     </div>
   );
