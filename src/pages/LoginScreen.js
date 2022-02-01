4f61f653cd22256c3f9a36b9196be996200a847e
Raymond <raymond@philippinedev.com>
2022-02-01 15:24:58 +0800

    Add Login page

---
+++ src/pages/LoginScreen.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import "../styles/LoginScreen.css";
+
+function LoginScreen() {
+  const [signIn, setSignIn] = useState(false);
+
+  return (
+    <div className="login-screen">
+      <div className="login-screen-bg">
+        <img
+          className="login-screen-logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="Netflix logo"
+        />
+        <button onClick={() => setSignIn(true)} className="sign-in-btn">
+          Sign In
+        </button>
+
+        <div className="login-screen-bg-gradient" />
+        <div className="login-screen-body">
+          <>
+            <h1 className="login-screen-title">
+              Unlimited films, TV <br />
+              programmes and more.
+            </h1>
+            <h2 className="login-screen-subtitle">
+              Watch anywhere. Cancel at any time.
+            </h2>
+            <h3 className="email-form-title">
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+            <div className="email-form-contents">
+              <form>
+                <input type="email" placeholder="Email address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="email-form-cta-btn"
+                >
+                  Get Started
+                </button>
+              </form>
+            </div>
+          </>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginScreen;
