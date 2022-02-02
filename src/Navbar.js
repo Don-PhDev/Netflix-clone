c2531342a5e2ac9a1a6523fb20313bd3c2ddadb8
Raymond <raymond@philippinedev.com>
2022-02-02 18:45:27 +0800

    Navigate to Home and Profile page

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,16 @@
 import "./styles/Navbar.css";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const [show, handleShow] = useState(false);
+  const navigate = useNavigate();
+  const goToProfile = () => {
+    navigate("/profile");
+  };
+  const goToHome = () => {
+    navigate("/");
+  };
 
   const transitionNavbar = () => {
     if (window.scrollY > 10) {
@@ -23,12 +31,14 @@ function Navbar() {
     <div className={`navbar ${show && "scrolled"}`}>
       <div className="navbar_contents">
         <img
+          onClick={goToHome}
           className="nav_logo"
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt="Netflix logo"
         />
 
         <img
+          onClick={goToProfile}
           className="nav_avatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
           alt="User avatar"
