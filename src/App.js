55551ed67d7c6d6b7d9a4405f7282097009abaad
Raymond <raymond@philippinedev.com>
2022-02-02 16:02:53 +0800

    Use Redux components

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import HomeScreen from "./pages/HomeScreen";
 import LoginScreen from "./pages/LoginScreen";
 import "./App.css";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
         console.log(userAuth);
       } else {
+        dispatch(logout);
       }
     });
 
