029ebe2b2adca27388a5b3fdfd1c9cbce3d30a11
Raymond <raymond@philippinedev.com>
2022-02-03 16:15:20 +0800

    Add movie trailer pop-ups

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "./axios";
 import "./styles/Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
   const baseUrl = "https://image.tmdb.org/t/p/original/";
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -15,6 +18,27 @@ function Row({ title, fetchUrl }) {
 
     fetchData();
   }, [fetchUrl]);
+
+  const opts = {
+    height: "1100",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.original_name || movie?.title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => alert(error.message));
+    }
+  };
 
   console.log(movies);
 
@@ -27,14 +51,16 @@ function Row({ title, fetchUrl }) {
           (movie) =>
             movie.backdrop_path && (
               <img
-                className="row_poster"
                 key={movie.id}
+                onClick={() => handleClick(movie)}
+                className="row_poster"
                 src={`${baseUrl}${movie.backdrop_path}`}
                 alt={movie.name}
               />
             )
         )}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
